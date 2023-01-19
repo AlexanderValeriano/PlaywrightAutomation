015ba61643dbe7180a4538393d66899b06e7ddb3
valeriano alexander <valerianoalexander@gmail.com>
2023-01-19 15:05:20 +0100

    13 how to work with locators which extract multiple elements in page

--- tests/UIBasicsTest.spec.js
+++ tests/UIBasicsTest.spec.js
@@ -1,18 +1,28 @@
 const { test, expect } = require("@playwright/test");
+const { use } = require("../playwright.config");
 
 test.only("Browser Context Playwright Test", async ({ browser }) => {
   //chrome - plugins / cookies;
+
   const context = await browser.newContext();
   const page = await context.newPage();
+  const userName = page.locator("#username");
+  const signIn = page.locator("#signInBtn");
   await page.goto("https://rahulshettyacademy.com/loginpagePractise/");
   console.log(await page.title());
   //css, xpath
-  await page.locator("#username").type("rahulshetty");
+  await userName.type("rahulshetty");
   await page.locator("#password").type("learning");
-  await page.locator("#signInBtn").click();
+  await signIn.click();
 
   console.log(await page.locator("[style*='block']").textContent());
   await expect(page.locator("[style*='block']")).toContainText("Incorrect");
+  //type - fill
+  await userName.fill("");
+  await userName.fill("rahulshettyacademy");
+  await signIn.click();
+  console.log(await page.locator(".card-body a").first().textContent());
+  console.log(await page.locator(".card-body a").nth(1).textContent());
 });
 
 test("Page Playwright Test", async ({ page }) => {
