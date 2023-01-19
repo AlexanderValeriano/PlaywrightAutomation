c214879383039e827f50e69d0e0acc9e385b7e62
valeriano alexander <valerianoalexander@gmail.com>
2023-01-19 11:37:04 +0100

    Browser context and page fixtures

---
+++ tests/UIBasicsTest.spec.js
@@ -0,0 +1,12 @@
+const { test } = require("@playwright/test");
+
+test("Browser Context Playwright Test", async ({ browser }) => {
+  chrome - plugins / ccookies;
+  const context = await browser.newContext();
+  const page = await context.newPage();
+  await page.goto("https://rahulshettyacademy.com/loginpagePractise/");
+});
+
+test("Page Playwright Test", async ({ page }) => {
+  await page.goto("https://rahulshettyacademy.com/loginpagePractise/");
+});
