39def639dae4f60e247815b4a9175b197e8d6e82
valeriano alexander <valerianoalexander@gmail.com>
2023-01-19 11:50:21 +0100

    Playwright configuration file and it's details to run the test

--- tests/UIBasicsTest.spec.js
+++ tests/UIBasicsTest.spec.js
@@ -1,12 +1,12 @@
 const { test } = require("@playwright/test");
 
 test("Browser Context Playwright Test", async ({ browser }) => {
-  chrome - plugins / ccookies;
+  //chrome - plugins / cookies;
   const context = await browser.newContext();
   const page = await context.newPage();
   await page.goto("https://rahulshettyacademy.com/loginpagePractise/");
 });
 
 test("Page Playwright Test", async ({ page }) => {
-  await page.goto("https://rahulshettyacademy.com/loginpagePractise/");
+  await page.goto("https://www.google.com");
 });
