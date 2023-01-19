9008a90f5f196a93f5530f8ecf31b6d524ad6430
valeriano alexander <valerianoalexander@gmail.com>
2023-01-19 14:42:53 +0100

    12 Extracting the text from browser and inserting valid expect assertions in test

--- tests/UIBasicsTest.spec.js
+++ tests/UIBasicsTest.spec.js
@@ -1,15 +1,18 @@
 const { test, expect } = require("@playwright/test");
 
-test("Browser Context Playwright Test", async ({ browser }) => {
+test.only("Browser Context Playwright Test", async ({ browser }) => {
   //chrome - plugins / cookies;
   const context = await browser.newContext();
   const page = await context.newPage();
   await page.goto("https://rahulshettyacademy.com/loginpagePractise/");
   console.log(await page.title());
   //css, xpath
-  page.locator("#username").type("rahulshetty");
-  page.locator("#password").type("learning");
-  page.locator("#signInBtn").click();
+  await page.locator("#username").type("rahulshetty");
+  await page.locator("#password").type("learning");
+  await page.locator("#signInBtn").click();
+
+  console.log(await page.locator("[style*='block']").textContent());
+  await expect(page.locator("[style*='block']")).toContainText("Incorrect");
 });
 
 test("Page Playwright Test", async ({ page }) => {
