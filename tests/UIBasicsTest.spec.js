7266ee5e736672d23aa881854c71773c697a7b91
valeriano alexander <valerianoalexander@gmail.com>
2023-02-03 18:08:47 +0100

    28 dynamically find the order from OrderHistory page using playwright Script Logic

--- tests/UIBasicsTest.spec.js
+++ tests/UIBasicsTest.spec.js
@@ -150,5 +150,18 @@ test.only("Client App login", async ({ page }) => {
     .locator(".em-spacer-1 .ng-star-inserted")
     .textContent();
   console.log(orderId);
+  await page.locator("button[routerlink*='myorders']").click();
+  await page.locator("tbody").waitFor();
+  const rows = await page.locator("tbody tr");
+
+  for (let i = 0; i < rows.count(); i++) {
+    const rowOrderId = await rows.nth(i).locator("th").textContent();
+    if (orderId.includes(rowOrderId)) {
+      await rows.nth(i).locator("button").first().click();
+      break;
+    }
+  }
+  const orderIdDetails = await page.locator(".col-text").textContent();
+  expect(orderId.includes(orderIdDetails)).toBeTruthy();
   // await page.pause();
 });
