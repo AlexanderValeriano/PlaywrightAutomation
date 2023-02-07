066bccfbf000f5b6d48942914d8cf7eda385a6ef
valeriano alexander <valerianoalexander@gmail.com>
2023-02-07 11:37:48 +0100

    31 How to handle and automate frames with playwright example

--- tests/MoreValidations.spec.js
+++ tests/MoreValidations.spec.js
@@ -10,10 +10,18 @@ test("Popup validations", async ({ page }) => {
   await page.locator("#hide-textbox").click();
   await expect(page.locator("#displayed-text")).toBeHidden();
   // PopUP or Dialog
-  await page.pause();
+  //   await page.pause();
   page.on("dialog", (dialog) => dialog.accept());
   //   page.on("dialog", (dialog) => dialog.dismiss());
   await page.locator("#confirmbtn").click();
   // Mouse hover in plywright
   await page.locator("#mousehover").hover();
+
+  // FRAMES
+
+  const framesPage = page.frameLocator("#courses-iframe");
+  await framesPage.locator(" li a[href*='lifetime-access']:visible").click();
+
+  const textCheck = await framesPage.locator(".text h2").textContent();
+  console.log(textCheck.split(" ")[1]);
 });
