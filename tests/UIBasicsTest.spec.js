2c446865561fcd1addb926c3897e33e7e167d44a
Alexander VALERIANO <valerianoalexander@gmail.com>
2023-02-13 16:51:04 +0100

    45 Part 1 How to abort the network calls with playwright Examples

--- tests/UIBasicsTest.spec.js
+++ tests/UIBasicsTest.spec.js
@@ -154,7 +154,7 @@ test.only("Client App login", async ({ page }) => {
   await page.locator("tbody").waitFor();
   const rows = await page.locator("tbody tr");
 
-  for (let i = 0; i < rows.count(); i++) {
+  for (let i = 0; i < (await rows.count()); i++) {
     const rowOrderId = await rows.nth(i).locator("th").textContent();
     if (orderId.includes(rowOrderId)) {
       await rows.nth(i).locator("button").first().click();
