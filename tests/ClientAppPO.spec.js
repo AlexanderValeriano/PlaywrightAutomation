d9c176231d7a4aafbf29f6f46e58d2792b951607
valeriano alexander <valerianoalexander@gmail.com>
2023-02-17 09:52:44 +0100

    52 How to drive the data from external json files to playwright tests

--- tests/ClientAppPO.spec.js
+++ tests/ClientAppPO.spec.js
@@ -1,22 +1,22 @@
 const { test, expect } = require("@playwright/test");
 const { POManager } = require("../pageobjects/POManager");
-
+// JSON => String => JS object
+const dataSet = JSON.parse(
+  JSON.stringify(require("../utils/placeOrderTestData.json"))
+);
 test.only("Client App login", async ({ page }) => {
   const poManager = new POManager(page);
   //js file - Login.js, DashboardPAge
-  const username = "valerianoalexander@gmail.com";
-  const password = "Petit_22$";
-  const productName = "zara coat 3";
   const products = page.locator(".card-body");
   const loginPage = poManager.getLoginPage();
   await loginPage.goTo();
-  await loginPage.validLogin(username, password);
+  await loginPage.validLogin(dataSet.username, dataSet.password);
   const dashboardPage = poManager.getDashboardPage();
-  await dashboardPage.searchProductAddCart(productName);
+  await dashboardPage.searchProductAddCart(dataSet.productName);
   await dashboardPage.navigateToCart();
 
   const cartPage = poManager.getCartPage();
-  await cartPage.VerifyProductIsDisplayed(productName);
+  await cartPage.VerifyProductIsDisplayed(dataSet.productName);
   await cartPage.Checkout();
 
   const ordersReviewPage = poManager.getOrdersReviewPage();
